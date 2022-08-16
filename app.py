b73b6b726d0e0a7f8794d24f0077aa1aacf15f62
RealYIsmail <YonisI2021@gmail.com>
2022-08-16 13:24:30 +0100

    Database integration

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = "Secret Key"
+
+## possibly error in URI
+app.config['SQLALCHEMY DATABASE_URI'] = 'mysql://root:''@localhost/todoflaskdb'
+app.config['SQLALCHEMY TRACK MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(100))
+
+    def __init__(self, task):
+        self.task = task
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
-
-
 
 
 if __name__ == "__main__":
