c5717a95778ef2199f388a85bee8d49b009bccfd
RealYIsmail <YonisI2021@gmail.com>
2022-08-17 20:11:48 +0100

    Database interactions

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.secret_key = "Secret Key"
 
 ## possibly error in URI
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/todoflaskdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost:3307/todoflaskdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -22,7 +22,9 @@ class Data(db.Model):
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    all_data = Data.query.all()
+
+    return render_template("index.html", listedtask = all_data)
 
 @app.route('/insert', methods = ['POST'])
 def insert():
@@ -35,7 +37,23 @@ def insert():
 
         return redirect(url_for('index'))
 
+@app.route('/edit', methods = ['GET', 'POST'])
+def edit():
 
+    if request.method == 'POST':
+        my_data = Data.query.get(request.form.get('id'))
+        my_data.task = request.form['task']
+
+        db.session.commit()
+        return redirect(url_for('index'))
+
+@app.route('/delete/<id>/', methods = ['GET', 'POST'])
+def delete(id):
+    my_data = Data.query.get(id)
+    db.session.delete(my_data)
+    db.session.commit()
+
+    return redirect (url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True)
